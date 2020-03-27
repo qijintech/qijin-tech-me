949e803b91864eb90ec7a04353c841eda712b184
michealyang <michealyang@aliyun.com>
2020-03-27 16:27:01 +0800

    1. 添加swagger 2. 调整port

--- me-uploader/src/main/java/tech/qijin/me/uploader/server/api/UploaderController.java
+++ me-uploader/src/main/java/tech/qijin/me/uploader/server/api/UploaderController.java
@@ -1,5 +1,6 @@
 package tech.qijin.me.uploader.server.api;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,18 +24,21 @@ public class UploaderController {
     @Autowired
     private UploaderService uploaderService;
 
+    @ApiOperation("上传文件，单个文件大小限制在100MB以内")
     @PostMapping("/file")
     public ResultVo uploadFile(@RequestParam("file") MultipartFile file, @RequestParam(required = false) String scene) throws IOException {
         return ResultVo.instance()
                 .data(uploaderService.uploadFile(file.getOriginalFilename(), file.getSize(), file.getInputStream(), Optional.ofNullable(scene)));
     }
 
+    @ApiOperation("上传图片，单个图片大小限制在5MB以内，支持的格式有jpg/jpeg/png/bmp/gif")
     @PostMapping("/image")
     public ResultVo uploadImage(@RequestParam("file") MultipartFile file, @RequestParam(required = false) String scene) throws IOException {
         return ResultVo.instance()
                 .data(uploaderService.uploadImage(file.getOriginalFilename(), file.getSize(), file.getInputStream(), Optional.ofNullable(scene)));
     }
 
+    @ApiOperation("上传音频，")
     @PostMapping("/audio")
     public ResultVo uploadAudio(@RequestParam("file") MultipartFile file) throws IOException {
         return null;
